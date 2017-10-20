820c43c12dbb4b6dfda169823e5b99b11665868c
Naveen Kumar <naveenrj50@gmail.com>
2017-10-20 18:07:22 +0530

    first commit

---
+++ src/com/emids/junit/calculator/model/Calculator.java
@@ -0,0 +1,7 @@
+package com.emids.junit.calculator.model;
+
+public class Calculator {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/com/emids/junit/calculator/services/CalculatorTest.java
@@ -0,0 +1,51 @@
+package com.emids.junit.calculator.services;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.emids.junit.calculator.model.Calculator;
+
+public class CalculatorTest {
+
+	@Test
+	 public static final void whenMoreThanTwoNumberThenThrowException()
+	 {
+		Calculator.add("1,2,3");
+	 }
+	@Test
+	public static final void whenEmptyStringIsUsedthenReturnValueIs0()
+	{
+		Assert.assertEquals(0, Calculator.add(""));
+	}
+	@Test
+	public static final void whenTwoNumberIsUsedThenReturnTheirSum()
+	{
+		Assert.assertEquals(46, Calculator.add("2,44"));
+	}
+	@Test
+	public static final void whenAnyNumberIsUsedThenReturnTheirSum()
+	{
+		Assert.assertEquals(2+3+4+5+6, Calculator.add("2,3,4,5,6"));
+	}
+	@Test
+	public static final void whenNewLineComesInBetweenThenReturnTheirSumOnly()
+	{
+		Assert.assertEquals(44+5+6, Calculator.add("44\n5\n6"));
+	}
+	@Test
+	public static final void whenDelimeterIsSpecifiedThenSeparateNumber()
+	{
+		Assert.assertEquals(3+6+15, Calculator.add("//;n3;6;15"));
+	}
+	@Test
+	public static final void whenNegativeNumberIsUsedThenThrownException()
+	{
+		Calculator.add("-1,-3,5");
+	}
+	@Test
+	public static final void whenOneOfTheNumberIsGreaterThan1000ThenShouldNotBeIncludedInsum()
+	{
+		Assert.assertEquals(2+6, Calculator.add("2,1000,1001,6,1234"));
+	}
+	
+	
+}
